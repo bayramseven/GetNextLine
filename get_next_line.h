afec71362bd95aa12755ce38ad9cc241d2ded4b0
Bayram <sevenbayramm@gmail.com>
2026-04-03 16:52:45 +0300

    dosya yapısını kurdum. .h dosyasını hazırladım. projede kullanmam gereken fonksiyonları vs belirledim. projede benden istenilen şeyleri kavradım. gerekli araştırmalarımı kaynaklardan araştırdım.

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bayram-seven <bayram-seven@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/03 15:12:53 by bayram-seve       #+#    #+#             */
+/*   Updated: 2026/04/03 16:51:14 by bayram-seve      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
